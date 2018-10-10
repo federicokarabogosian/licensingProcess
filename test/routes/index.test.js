85c75f765c07a5e17ed69b83bbaa1055bd3b35f5
Federico Karabogosian <karabogosian@simbius.com>
2018-10-10 20:26:24 -0300

    Se agrega las librerias de testing y una prueba simple para confirmar su correcto funcionamiento. Realizar la prueba ejecutando mocha ./test --recursive --timeout 15000

---
+++ test/routes/index.test.js
@@ -0,0 +1,25 @@
+'use stricts'
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+var server = require('../../server');
+
+chai.use(chaiHttp);
+const url= 'http://localhost:3000';
+
+describe('Index Route', function() {
+  describe('GET Index', function() {
+    it('Should get a response from index page', function() {
+      chai.request(url)
+      .get('/')
+      .end( function(err,res){
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
+        done();
+      })
+    })
+  })
+})
