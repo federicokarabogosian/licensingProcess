5f8ed5f50ddf9941c04c2d034406023e1cea270a
Federico Karabogosian <karabogosian@simbius.com>
2018-10-10 17:42:46 -0300

    Primer ejecutable del proyecto utilizando Express. Responde el GET / con status 200 y HTML

---
+++ app.js
@@ -0,0 +1,26 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+// Routers
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// Load middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//
+app.use('/', indexRouter);
+
+module.exports = app;
