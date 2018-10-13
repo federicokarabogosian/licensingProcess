359962d641b20ddfb31d91749d977a62f6304d09
Federico Karabogosian <karabogosian@simbius.com>
2018-10-13 15:47:11 -0300

    Se corrije el nombre del elemento de Movie y se agrega su obligatoriedad

--- models/track.js
+++ models/track.js
@@ -8,7 +8,7 @@ var trackSchema = Schema({
   name: {type: String, unique: true, required: [true,'The track must have a name']},
   startTime: {type: Number, min: 0, required: [true,'The track must have a start time']},
   endTime: {type: Number, min: 0, required: [true,'The track must have a end time']},
-  movieId: {type: Schema.Types.ObjectId, ref: 'Movie' }
+  movie: {type: Schema.Types.ObjectId, ref: 'Movie', required: [true,'The track must be associated with a movie']}
 }, {timestamps: true});
 
 module.exports = mongoose.model('Track',trackSchema);
