f9c7dbf6edd8e28c4582bcd4c9bfb6e141987bc4
Federico Karabogosian <karabogosian@simbius.com>
2018-10-13 15:14:14 -0300

    Esquema de Movie

--- models/movie.js
+++ models/movie.js
@@ -6,9 +6,8 @@ var Schema = mongoose.Schema;
 
 var movieSchema = Schema({
   name: {type: String, unique: true, required: [true,'The movie must have a name']},
-  director: {type: String, unique: true, required: [true,'The movie must have a director']},
+  director: {type: String, required: [true,'The movie must have a director']},
   year: {type: Number, min: 1900},
-  timestamps
-});
+}, {timestamps: true});
 
 module.exports = mongoose.model('Movie',movieSchema);
