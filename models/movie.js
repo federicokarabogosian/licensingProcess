c1d10cfae46f89df97549d2adea463fe6f32a8b1
Federico Karabogosian <karabogosian@simbius.com>
2018-10-13 13:50:18 -0300

    Se crea el esquema de Movie

---
+++ models/movie.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var movieSchema = Schema({
+  name: {type: String, unique: true, required: [true,'The movie must have a name']},
+  director: {type: String, unique: true, required: [true,'The movie must have a director']},
+  year: {type: Number, min: 1900},
+  timestamps
+});
+
+module.exports = mongoose.model('Movie',movieSchema);
