b86f982ca280d2dcf4bf3f74b88326106f051570
Federico Karabogosian <karabogosian@simbius.com>
2018-10-15 01:40:40 -0300

    Se agregan los headers CORDS para que la API pueda ser utilizado por la aplicación web

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ var songRouter = require('./routes/song');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 // Load middleware
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
